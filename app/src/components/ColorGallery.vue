f332bc984e352b86562eaf812d0514fc288cf145
Stuart Allen <stuart.lee.allen@gmail.com>
2024-07-14 00:40:36 -0700

    Basically completed UI

--- app/src/components/ColorGallery.vue
+++ app/src/components/ColorGallery.vue
@@ -18,24 +18,34 @@
     </div>
     <hr />
     <div
-      className="flex flex-wrap justify-center overflow-x-hidden overflow-y-auto"
+      className="flex flex-wrap justify-center overflow-x-hidden overflow-y-auto pb-8"
     >
       <div
         v-for="color in colors"
         :key="color.closestNamedHex"
-        className="w-fit h-fit flex flex-col justify-center items-center ml-4 mt-4 border-2 p-4 border-black rounded-lg shadow-lg hover:mt-2"
+        className="w-fit h-fit flex flex-col justify-center items-center ml-4 mt-4 border-2 px-4 pb-4 py-2 border-black rounded-lg shadow-lg hover:mt-2"
       >
         <div
-          :style="{ backgroundColor: color.closestNamedHex }"
-          className="w-16 h-16 rounded-full"
+          :style="{
+            backgroundColor: color.closestNamedHex,
+          }"
+          className="w-20 h-20 rounded-full border-2 border-gray-200"
         />
         <h3 className="font-bold">{{ color.value }}</h3>
         <span>{{ color.closestNamedHex }}</span>
         <span>({{ color.r }} {{ color.g }} {{ color.b }})</span>
+        <button
+          @click="copyColorProperties(color)"
+          className="px-2 py-1 mt-1 bg-blue-500 text-white rounded-lg hover:bg-blue-700"
+          title="Copy to Clipboard"
+          :aria-label="`Copy ${color.value} to Clipboard`"
+        >
+          Copy
+        </button>
       </div>
     </div>
-    <hr className="my-16" />
   </div>
+  <hr className="mb-16" />
 </template>
 
 <script lang="ts">
@@ -50,6 +60,17 @@ export default defineComponent({
     isLoading: { type: Boolean, required: true },
     hasError: { type: Boolean, required: true },
     colors: { type: Array as () => ColorCacheEntry[], required: false },
+  },
+  methods: {
+    async copyColorProperties(color: ColorCacheEntry) {
+      const colorProperties = `${color.value}, ${color.closestNamedHex}, (${color.r}, ${color.g}, ${color.b})`;
+      try {
+        await navigator.clipboard.writeText(colorProperties);
+        console.log("Color properties copied to clipboard");
+      } catch (err) {
+        console.error("Failed to copy color properties", err);
+      }
+    },
   },
 });
 </script>
